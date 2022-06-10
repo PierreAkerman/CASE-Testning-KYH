7b0ff9539ab3bb7539f55d9caf1ac9f40e36422c
Pierre Åkerman <pirreakerman@gmail.com>
2022-06-10 10:21:42 +0200

    Added HomeController Tests, added CategoryService Tests, added 2 tests for my method AgreementIsValid, that was implemented in CalculatePrice to exclude invalid agreements from beeing processed.

---
+++ SuperShopTestProject/Services/CategoryServiceTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcSuperShop.Data;
+using MvcSuperShop.Services;
+
+
+namespace SuperShopTestProject.Services
+{
+    [TestClass]
+    public class CategoryServiceTests
+    {
+        private CategoryService sut;
+        private ApplicationDbContext context;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            context = new ApplicationDbContext(contextOptions);
+            context.Database.EnsureCreated();
+
+            sut = new CategoryService(context);
+        }
+
+        [TestMethod]
+        public void When_get_trending_categories_should_return_correct_number_of_categories()
+        {
+            //ARRANGE
+            context.Categories.Add(new Category
+            {
+                Name = "Van",
+                Icon = "lalala"
+            });
+            context.Categories.Add(new Category
+            {
+                Name = "Hybrid",
+                Icon = "lalala"
+            });
+            context.Categories.Add(new Category
+            {
+                Name = "Suv",
+                Icon = "lalala"
+            });
+            context.Categories.Add(new Category
+            {
+                Name = "Wagon",
+                Icon = "lalala"
+            });
+            context.Categories.Add(new Category
+            {
+                Name = "Coupe",
+                Icon = "lalala"
+            });
+            context.SaveChanges();
+            //ACT
+            var result = sut.GetTrendingCategories(3);
+            //ASSERT
+            Assert.AreEqual(3, result.Count());
+        }
+        [TestMethod]
+        public void When_get_to_many_trending_categories_should_return_all_existing_categories()
+        {
+            //ARRANGE
+            context.Categories.Add(new Category
+            {
+                Name = "Van",
+                Icon = "lalala"
+            });
+            context.Categories.Add(new Category
+            {
+                Name = "Hybrid",
+                Icon = "lalala"
+            });
+            context.Categories.Add(new Category
+            {
+                Name = "Suv",
+                Icon = "lalala"
+            });
+            context.Categories.Add(new Category
+            {
+                Name = "Wagon",
+                Icon = "lalala"
+            });
+            context.Categories.Add(new Category
+            {
+                Name = "Coupe",
+                Icon = "lalala"
+            });
+
+            context.SaveChanges();
+            //ACT
+            var result = sut.GetTrendingCategories(6);
+            //ASSERT
+            Assert.AreEqual(5, result.Count());
+        }
+    }
+}
--- SuperShopTestProject/Services/PricingServiceTests.cs
+++ SuperShopTestProject/Services/PricingServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -20,7 +21,11 @@ namespace SuperShopTestProject.Services
         {
             _sut = new PricingService();
         }
-
+        public CurrentCustomerContext CreateCustomer()
+        {
+            var customer = new CurrentCustomerContext();
+            return customer;
+        }
         [TestMethod]
         public void When_no_agreement_exists_product_basePrice_is_used()
         {
@@ -68,7 +73,6 @@ namespace SuperShopTestProject.Services
                             {
                                 Id = 1,
                                 CategoryMatch = "van",
-                                ManufacturerMatch = "Bugatti",
                                 PercentageDiscount = 6.0m
                             }
                         }
@@ -81,7 +85,7 @@ namespace SuperShopTestProject.Services
             Assert.AreEqual(94, products.First().Price);
         }
         [TestMethod]
-        public void When_agreement_is_Not_valid_discount_should_Not_be_used_for_lower_price()
+        public void When_agreement_is_not_valid_discount_should_not_be_used_for_lower_price()
         {
             //ARRANGE
             var productList = new List<ProductServiceModel>
@@ -121,7 +125,7 @@ namespace SuperShopTestProject.Services
             Assert.AreEqual(100, products.First().Price);
         }
         [TestMethod]
-        public void When_multiple_agreements_exists_the_lowest_price_should_be_used()
+        public void When_multiple_agreements_rows_the_lowest_price_should_be_used()
         {
             //ARRANGE
             var productList = new List<ProductServiceModel>
@@ -150,9 +154,40 @@ namespace SuperShopTestProject.Services
                                 CategoryMatch = "van",
                                 ManufacturerMatch = "Bugatti",
                                 PercentageDiscount = 6.0m
+                            },
+                            new AgreementRow
+                            {
+                                Id = 2,
+                                ProductMatch = "hybrid",
+                                ManufacturerMatch = "Bugatti",
+                                PercentageDiscount = 5.0m
                             }
                         }
                     },
+                }
+            };
+            //ACT
+            var products = _sut.CalculatePrices(productList, customerContext);
+            //ASSERT
+            Assert.AreEqual(94, products.First().Price);
+        }
+        [TestMethod]
+        public void When_multiple_agreements_exists_the_lowest_price_should_be_used()
+        {
+            //ARRANGE
+            var productList = new List<ProductServiceModel>
+            {
+                new ProductServiceModel{
+                    BasePrice = 100,
+                    Name = "XTS Hybrid",
+                    CategoryName = "van",
+                    ManufacturerName = "Bugatti",
+                }
+            };
+            var customerContext = new CurrentCustomerContext
+            {
+                Agreements = new List<Agreement>
+                {
                     new Agreement
                     {
                         Id = 1,
@@ -162,10 +197,36 @@ namespace SuperShopTestProject.Services
                         {
                             new AgreementRow
                             {
+                                Id = 1,
+                                CategoryMatch = "van",
+                                PercentageDiscount = 6.0m
+                            },
+                            new AgreementRow
+                            {
                                 Id = 2,
                                 ProductMatch = "hybrid",
-                                ManufacturerMatch = "Bugatti",
                                 PercentageDiscount = 5.0m
+                            }
+                        }
+                    },
+                    new Agreement
+                    {
+                        Id = 2,
+                        ValidFrom = DateTime.Now.Date.AddDays(-5),
+                        ValidTo = DateTime.Now.Date.AddDays(5),
+                        AgreementRows = new List<AgreementRow>
+                        {
+                            new AgreementRow
+                            {
+                                Id = 3,
+                                CategoryMatch = "volvo",
+                                PercentageDiscount = 10.0m
+                            },
+                            new AgreementRow
+                            {
+                                Id = 4,
+                                ProductMatch = "hybrid",
+                                PercentageDiscount = 4.0m
                             }
                         }
                     }
@@ -196,7 +257,6 @@ namespace SuperShopTestProject.Services
                 {
                     new Agreement
                     {
-                        Id = 1,
                         ValidFrom = DateTime.Now.Date.AddDays(-5),
                         ValidTo = DateTime.Now.Date.AddDays(-3),
                         AgreementRows = new List<AgreementRow>
@@ -205,14 +265,12 @@ namespace SuperShopTestProject.Services
                             {
                                 Id = 1,
                                 CategoryMatch = "van",
-                                ManufacturerMatch = "Bugatti",
                                 PercentageDiscount = 6.0m
                             }
                         }
                     },
                     new Agreement
                     {
-                        Id = 1,
                         ValidFrom = DateTime.Now.Date.AddDays(-5),
                         ValidTo = DateTime.Now.Date.AddDays(5),
                         AgreementRows = new List<AgreementRow>
@@ -221,7 +279,6 @@ namespace SuperShopTestProject.Services
                             {
                                 Id = 2,
                                 ProductMatch = "hybrid",
-                                ManufacturerMatch = "Bugatti",
                                 PercentageDiscount = 5.0m
                             }
                         }
@@ -231,6 +288,41 @@ namespace SuperShopTestProject.Services
             var products = _sut.CalculatePrices(productList, customerContext);
             //ASSERT
             Assert.AreEqual(95, products.First().Price);
+        }
+
+        [TestMethod]
+        public void When_agreement_is_valid_should_return_true()
+        {
+            var customer = CreateCustomer();
+
+            customer.Agreements = new List<Agreement>
+            {
+                new Agreement
+                {
+                    ValidFrom = DateTime.Now.Date.AddDays(-5),
+                    ValidTo = DateTime.Now.Date.AddDays(5),
+                }
+            };
+
+            var result = _sut.AgreementIsValid(customer.Agreements.First());
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void When_agreement_is_invalid_should_return_false()
+        {
+            var customer = CreateCustomer();
+
+            customer.Agreements = new List<Agreement>
+            {
+                new Agreement
+                {
+                    ValidFrom = DateTime.Now.Date.AddDays(-15),
+                    ValidTo = DateTime.Now.Date.AddDays(-5),
+                }
+            };
+
+            var result = _sut.AgreementIsValid(customer.Agreements.First());
+            Assert.IsFalse(result);
         }
     }
 }
